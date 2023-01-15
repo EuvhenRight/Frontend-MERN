79417a059567e50367d7a005500cfdc3b7e6a7c2
EuvhenRight <ugnivenko.ea@gmail.com>
2023-01-15 02:03:45 +0100

    change text

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -53,14 +53,14 @@ export const FullPost = () => {
         items={[
           {
             user: {
-              fullName: "Вася Пупкин",
+              fullName: "User_1",
               avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
             },
-            text: "Это тестовый комментарий 555555",
+            text: "This is a test comment",
           },
           {
             user: {
-              fullName: "Иван Иванов",
+              fullName: "User_2",
               avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
             },
             text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -65,14 +65,14 @@ export const Home = () => {
             items={[
               {
                 user: {
-                  fullName: "Вася Пупкин",
+                  fullName: "User_1",
                   avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
                 },
-                text: "Это тестовый комментарий",
+                text: "Test comments",
               },
               {
                 user: {
-                  fullName: "Иван Иванов",
+                  fullName: "User_2",
                   avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
                 },
                 text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
